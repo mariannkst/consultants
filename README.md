15484f2852a352d850f1eb9ff4d894063e98f13f
mariannK <karsz@DESKTOP-O3C7C87>
2021-03-10 04:28:52 +0100

    Extended the API result with Titles and WorkExperience

--- README.md
+++ README.md
@@ -4,7 +4,25 @@ This is a full stack solution with Angular frontend, APIs and Database
 The solution is made as a job application test for Flextribe.
 
 Parts:
+
 frontend - the Angular SPA
+
 backend/api - .NET Core API
+
 backend/database - the set up of the SQL database
+
+
+Instructions for trying out the backend:
+
+1. Import the database to your local SQL Server
+2. Go to /backend/api/webapi folder and open the webapi.sln
+3. Go to the appsettings.json file and update the "ConnectionStrings.ConsultantApiConnection" so it matches your own database
+4. Build the solution
+5. Run the application
+6. Open Postman and go to http://localhost:5000/api/Consultant/{id}
+	Where 'id' is the id of the Consultant
+	(Check your localhost port, you might need to adjust it)
+
+
+
 
--- backend/api/webapi/webapi/Models/Consultant.cs
+++ backend/api/webapi/webapi/Models/Consultant.cs
@@ -5,15 +5,19 @@ using System.Threading.Tasks;
 
 namespace webapi.Models
 {
-    public class Consultant
-    {
+	public class Consultant
+	{
 		public int ConsultantId { get; set; }
 
 		public string FirstName { get; set; }
 
 		public string LastName { get; set; }
 
-		public int CityId { get; set; }
+		public string CityName { get; set; }
+
+		public string StateAbbreviation { get; set; }
+
+		public string CountryAbbreviation { get; set; }
 
 		public int HourlyRateShortProject { get; set; }
 
@@ -23,5 +27,25 @@ namespace webapi.Models
 
 		public string ProfilePicture { get; set; }
 
+		public List<string> Titles { get; set; }
+		public List<WorkExperienceDetails> WorkExperience { get; set; }
+
+
+
+    }
+
+	public class WorkExperienceDetails
+	{
+		public string JobTitle { get; set; }
+
+		public string CompanyName { get; set; }
+
+		public DateTime StartDate { get; set; }
+
+		public DateTime EndDate { get; set; }
+
+		public string Description { get; set; }
+
+		public string IndustryName { get; set; }
 	}
 }
