a035cafe5599f8a06083a18a12a7100605968081
mariannK <karsz@DESKTOP-O3C7C87>
2021-03-10 07:34:55 +0100

    Add components template

--- frontend/consultants-app/src/app/app.module.ts
+++ frontend/consultants-app/src/app/app.module.ts
@@ -3,14 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { ConsultantComponent } from './consultant/consultant.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    ConsultantComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+     // { path: 'consultants', component: my-consultants },
+      { path: 'consultants/:consultantId', component: ConsultantComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
